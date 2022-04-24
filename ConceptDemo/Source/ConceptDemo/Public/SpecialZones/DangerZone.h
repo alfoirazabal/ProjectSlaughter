a7fee7550678c231cc85a0ceaac0c9772b1f1a50
Alfonso Irazabal <alfoirazabal@live.com>
2022-04-24 00:08:11 -0300

    Danger zones now with needles and slowing down characters.

--- ConceptDemo/Source/ConceptDemo/Public/SpecialZones/DangerZone.h
+++ ConceptDemo/Source/ConceptDemo/Public/SpecialZones/DangerZone.h
@@ -3,8 +3,15 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "ConceptDemoPaperCharacter.h"
 #include "Engine/StaticMeshActor.h"
 #include "DangerZone.generated.h"
+
+USTRUCT() struct FCharacterDefaultWalkSpeeds {
+	GENERATED_BODY()
+	UPROPERTY() AUConceptDemoPaperCharacter* Character;
+	float DefaultWalkSpeed;
+};
 
 /**
  * 
@@ -16,12 +23,23 @@ class CONCEPTDEMO_API ADangerZone : public AStaticMeshActor
 
 public:
 
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE(FBeginOverlapEvent);
+
+	TArray<FCharacterDefaultWalkSpeeds> OverlappingCharactersDefaultWalkSpeeds;
 	UPROPERTY(EditAnywhere) float DamageHitDeltaSeconds;
 	UPROPERTY(EditAnywhere) float ZoneDamage;
+	UPROPERTY(EditAnywhere) float CharacterMovementSlowdownAmount;
 	float CurrentDamageHitDeltaSeconds;
 	
 	virtual void Tick(float DeltaSeconds) override;
+
+	int FindCharacterWalkSpeedIndex(const AUConceptDemoPaperCharacter* Character);
 	
 	ADangerZone();
+
+	UPROPERTY(VisibleAnywhere, Category = "Trigger Box")
+	class UBoxComponent* TriggerBox;
+	UFUNCTION() void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	UFUNCTION() void OnOverlapEnd(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 	
 };

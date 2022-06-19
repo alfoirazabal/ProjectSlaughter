ac434777b9e498796e758a1191c797f4f4981eab
Alfonso <alfoirazabal@live.com>
2022-06-19 00:43:24 -0300

    New Police Space Ship + Spawning chances for ships.

--- ConceptDemo/Source/ConceptDemo/Private/Vehicles/SpaceShip/SpaceShipSpawner.cpp
+++ ConceptDemo/Source/ConceptDemo/Private/Vehicles/SpaceShip/SpaceShipSpawner.cpp
@@ -24,10 +24,39 @@ void ASpaceShipSpawner::BeginPlay()
 	Super::BeginPlay();
 	const float RandomSpawnTime = FMath::RandRange(this->SpawnMinTime, this->SpawnMaxTime);
 	FTimerHandle TimerHandle;
+	this->FillRandomVehicleTypes();
 	this->GetWorld()->GetTimerManager().SetTimer(TimerHandle, this, &ASpaceShipSpawner::SpawnVehicle, RandomSpawnTime, false);
 	if (this->StartWithSpawnedVehicles) 
 	{
 		this->SpawnStartupVehicles();
+	}
+}
+
+void ASpaceShipSpawner::FillRandomVehicleTypes()
+{
+	float VehicleTypesProbabilitySum = 0;
+	TArray<ASpaceShip*> RandomVehicles;
+	for (int i = 0 ; i < this->VehicleTypes.Num() ; i++)
+	{
+		const TSubclassOf<ASpaceShip> CurrentVehicleType = this->VehicleTypes[i];
+		ASpaceShip* CurrentRandomVehicle = Cast<ASpaceShip>(CurrentVehicleType->GetDefaultObject());
+		if (CurrentRandomVehicle)
+		{
+			VehicleTypesProbabilitySum += CurrentRandomVehicle->SpawnChance;
+			RandomVehicles.Add(CurrentRandomVehicle);
+		}
+	}
+	this->RandomVehicleTypes.Reserve(100);
+	for (int i = 0 ; i < 100 ; i++)
+	{
+		for (int j = 0 ; j < RandomVehicles.Num() ; j++)
+		{
+			const int TimesToAdd = (RandomVehicles[j]->SpawnChance / VehicleTypesProbabilitySum) * 100;
+			for (int k = i ; k < TimesToAdd + i ; k++)
+			{
+				RandomVehicleTypes.Add(this->VehicleTypes[j]);
+			}
+		}
 	}
 }
 
@@ -37,38 +66,64 @@ void ASpaceShipSpawner::Tick(const float DeltaTime)
 	Super::Tick(DeltaTime);
 }
 
+TSubclassOf<ASpaceShip> ASpaceShipSpawner::GetRandomSpaceShipType()
+{
+	TSubclassOf<ASpaceShip> RandomVehicleType = nullptr;
+	if (this->RandomVehicleTypes.Num() > 0)
+	{
+		const int RandomPosition = FMath::RandRange(0, 100);
+		RandomVehicleType = this->RandomVehicleTypes[RandomPosition];
+	}
+	return RandomVehicleType;
+}
+
 void ASpaceShipSpawner::SpawnVehicle()
 {
 	const FTransform Transform = this->GetActorTransform();
-	ASpaceShip* NewShip = this->GetWorld()->SpawnActorDeferred<ASpaceShip>(this->VehicleType, Transform, this, nullptr, ESpawnActorCollisionHandlingMethod::AlwaysSpawn);
-	NewShip->StartupRotation = this->StartupRotation;
-	NewShip->TravelVelocity = this->TravelVelocity;
-	NewShip->TravelDistance = this->TravelDistance;
-	UGameplayStatics::FinishSpawningActor(NewShip, Transform);
-	const float RandomSpawnTime = FMath::RandRange(this->SpawnMinTime, this->SpawnMaxTime);
-	FTimerHandle TimerHandle;
-	this->GetWorld()->GetTimerManager().SetTimer(TimerHandle, this, &ASpaceShipSpawner::SpawnVehicle, RandomSpawnTime, false);
-}
-
-void ASpaceShipSpawner::SpawnStartupVehicles() 
-{
-	float CurrentSpawnedDistance = 0;
-	while (
-		(this->TravelVelocity > 0 && CurrentSpawnedDistance <= this->TravelDistance) ||
-		(this->TravelVelocity < 0 && CurrentSpawnedDistance >= -this->TravelDistance)
-	)
+	const TSubclassOf<ASpaceShip> RandomSpaceShipType = this->GetRandomSpaceShipType();
+	if (RandomSpaceShipType)
 	{
-		const float RandomSpawnDistance = (FMath::RandRange(this->SpawnMinTime, this->SpawnMaxTime) * this->TravelVelocity) + CurrentSpawnedDistance;
-		FVector SpawnPosition = this->GetActorLocation();
-		SpawnPosition.Y += RandomSpawnDistance;
-		const FTransform Transform = this->GetActorTransform();
-		ASpaceShip* NewShip = this->GetWorld()->SpawnActorDeferred<ASpaceShip>(this->VehicleType, Transform, this, nullptr, ESpawnActorCollisionHandlingMethod::AlwaysSpawn);
+		ASpaceShip* NewShip = this->GetWorld()->SpawnActorDeferred<ASpaceShip>(RandomSpaceShipType, Transform, this, nullptr, ESpawnActorCollisionHandlingMethod::AlwaysSpawn);
 		NewShip->StartupRotation = this->StartupRotation;
 		NewShip->TravelVelocity = this->TravelVelocity;
 		NewShip->TravelDistance = this->TravelDistance;
 		UGameplayStatics::FinishSpawningActor(NewShip, Transform);
-		NewShip->SetActorLocation(SpawnPosition);
-		CurrentSpawnedDistance += RandomSpawnDistance;
+		const float RandomSpawnTime = FMath::RandRange(this->SpawnMinTime, this->SpawnMaxTime);
+		FTimerHandle TimerHandle;
+		this->GetWorld()->GetTimerManager().SetTimer(TimerHandle, this, &ASpaceShipSpawner::SpawnVehicle, RandomSpawnTime, false);
 	}
-	
+	else
+	{
+		GEngine->AddOnScreenDebugMessage(783164382, 4, FColor::Red, "No space ship types to spawn on SpaceShipSpawner");
+	}
+}
+
+void ASpaceShipSpawner::SpawnStartupVehicles() 
+{
+	if (this->RandomVehicleTypes.Num() > 0)
+	{
+		float CurrentSpawnedDistance = 0;
+		while (
+			(this->TravelVelocity > 0 && CurrentSpawnedDistance <= this->TravelDistance) ||
+			(this->TravelVelocity < 0 && CurrentSpawnedDistance >= -this->TravelDistance)
+		)
+		{
+			const float RandomSpawnDistance = (FMath::RandRange(this->SpawnMinTime, this->SpawnMaxTime) * this->TravelVelocity) + CurrentSpawnedDistance;
+			FVector SpawnPosition = this->GetActorLocation();
+			SpawnPosition.Y += RandomSpawnDistance;
+			const FTransform Transform = this->GetActorTransform();
+			const TSubclassOf<ASpaceShip> RandomSpaceShipType = this->GetRandomSpaceShipType();
+			ASpaceShip* NewShip = this->GetWorld()->SpawnActorDeferred<ASpaceShip>(RandomSpaceShipType, Transform, this, nullptr, ESpawnActorCollisionHandlingMethod::AlwaysSpawn);
+			NewShip->StartupRotation = this->StartupRotation;
+			NewShip->TravelVelocity = this->TravelVelocity;
+			NewShip->TravelDistance = this->TravelDistance;
+			UGameplayStatics::FinishSpawningActor(NewShip, Transform);
+			NewShip->SetActorLocation(SpawnPosition);
+			CurrentSpawnedDistance += RandomSpawnDistance;
+		}
+	}
+	else
+	{
+		GEngine->AddOnScreenDebugMessage(783164382, 4, FColor::Red, "No space ship types to spawn on SpaceShipSpawner");
+	}
 }

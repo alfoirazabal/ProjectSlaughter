98d174afb4debdc23d0b4e9cf0cf15622ce4bad3
Alfonso <alfoirazabal@live.com>
2022-06-28 20:32:33 -0300

    Uploading Windows Launcher V1
    This launcher allows you to graphically change the settings (currently only the setting to change whether to be able to use a controller for P2 or not).

---
+++ Launcher/Slaughter Friends Launcher/Logic/Helpers/IniKeyValuePairsBuilder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slaughter_Friends_Launcher.Logic.Helpers
+{
+    internal class IniKeyValuePairsBuilder
+    {
+        public string BuildBooleanKey(string key, bool booleanTrue)
+        {
+            string keyValuePairStringed;
+            if (booleanTrue)
+            {
+                keyValuePairStringed = key + "=1";
+            }
+            else
+            {
+                keyValuePairStringed = key + "=0";
+            }
+            return keyValuePairStringed;
+        }
+
+    }
+}
---
+++ Launcher/Slaughter Friends Launcher/MainForm.cs
@@ -0,0 +1,63 @@
+﻿using Slaughter_Friends_Launcher.Logic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Slaughter_Friends_Launcher
+{
+    public partial class MainForm : Form
+    {
+
+        private readonly SettingsFileLogic SettingsFileLogic;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            this.LblLauncherVersion.Text = "1.0";
+            this.SettingsFileLogic = new SettingsFileLogic();
+            this.CbxUseControllerForP2.Checked = this.SettingsFileLogic.IsUseControllerForP2Enabled();
+        }
+
+        private void CbxUseControllerForP2_CheckedChanged(object sender, EventArgs e)
+        {
+            this.SettingsFileLogic.SetUseControllerForP2Enabled(this.CbxUseControllerForP2.Checked);
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            string startupPath = Application.StartupPath;
+            string gamePath = startupPath + "\\" + ConfigurationManager.AppSettings["ExecutableRelativePath"];
+            try
+            {
+                Process.Start(gamePath);
+                Application.Exit();
+            }
+            catch (Exception exception)
+            {
+                if (exception is Win32Exception || exception is FileNotFoundException)
+                {
+                    MessageBox.Show(
+                        this,
+                        "Could not find file: " + gamePath + "\nCheck variable 'ExecutableRelativePath' on App.config",
+                        "Error",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error
+                    );
+                }
+                else
+                {
+                    throw exception;
+                }
+            }
+        }
+    }
+}
